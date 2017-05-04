ca4ad33dc06bbc238379554c130ba7d7f754e5e1
Sean Carey <wycleffsean@gmail.com>
2017-05-04 10:46:24 -0400

    Add fzf

---
+++ home/.zsh/after/fzf.zsh
@@ -0,0 +1,10 @@
+# Auto-completion
+if [[ -e "/usr/local/opt/fzf/shell/completion.zsh" ]]; then
+ [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+fi
+
+if [[ -e "/usr/local/opt/fzf/shell/key-bindings.zsh" ]]; then
+ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+fi
+
+export FZF_DEFAULT_COMMAND='ag -g "" --hidden --ignore ".git/*"'
